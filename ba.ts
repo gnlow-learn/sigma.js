351a6155411711cae3411bb36e9ea5756523a1ca
Gnlow <gnlow@protonmail.com>
2024-04-05 10:53:04 +0900

    ba m=2

--- ba.ts
+++ ba.ts
@@ -9,12 +9,33 @@ const randHash =
 () =>
 Math.random().toString(36).substring(2, 7)
 
-export const ba =
+const randNode =
 (graph: Graph) => {
     const randEdge = randItem(graph.edges())
     const source = graph.source(randEdge)
     const target = graph.target(randEdge)
     const randNode = randItem([source, target])
+
+    return randNode
+}
+
+const randNodes =
+(n: number) =>
+(graph: Graph) => {
+    const result: string[] = []
+    for (let i=0; i<n; i++) {
+        let node: string
+        while (true) {
+            node = randNode(graph)
+            if (!result.includes(node)) break
+        }
+        result.push(node)
+    }
+    return result
+}
+
+export const ba =
+(graph: Graph) => {
 
     const newNodeId = randHash()
 
@@ -23,5 +44,8 @@ export const ba =
         y: Math.random()*20,
         size: 5
     })
-    graph.addEdge(randNode, newNodeId)
+
+    randNodes(2)(graph).forEach(
+        target => graph.addEdge(newNodeId, target)
+    )
 }
